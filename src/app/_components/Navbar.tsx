0ecc0eadb8fdb20ba87e4f707502478c80fe5e06
William East <wjbetech@gmail.com>
2024-04-26 15:46:27 +0900

    Added UserButton on sign in

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -3,30 +3,30 @@ import {
 	SignOutButton,
 	SignedIn,
 	SignedOut,
+	UserButton,
 } from "@clerk/nextjs";
 import React from "react";
 
 export default function Navbar() {
 	return (
-		<nav className="flex w-full items-center justify-between p-4">
+		<nav className="flex w-full items-center justify-between p-4 align-baseline">
 			<div>
 				<h1 className="text-xl font-semibold">T3 Gallery</h1>
 			</div>
 			<div>
 				<input type="text" />
 			</div>
-			<div className="">
+			<div>
 				<ul className="flex gap-4">
-					<li>
-						<a href="/">Home</a>
-					</li>
-					<div>
+					<div className="flex">
 						<SignedOut>
 							<SignInButton />
 						</SignedOut>
-						<SignedIn>
-							<SignOutButton />
-						</SignedIn>
+						<div className="gap-2">
+							<SignedIn>
+								<UserButton showName />
+							</SignedIn>
+						</div>
 					</div>
 				</ul>
 			</div>
