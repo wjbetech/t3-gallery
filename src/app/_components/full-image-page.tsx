88ac0fd931ac55b7a641b288346e690889fcf9cd
William East <wjbetech@gmail.com>
2024-05-28 16:16:22 +0900

    Fixed stash, modal into refresh still not working

--- src/app/_components/full-image-page.tsx
+++ src/app/_components/full-image-page.tsx
@@ -4,5 +4,16 @@ export default async function FullPageImageView(props: {
 	id: number;
 }) {
 	const image = await getImage(props.id);
-	return <img src={image.url} className="w-96" alt="" />;
+	return (
+		<div className="flex w-full bg-green-500">
+			<img src={image.url} className="w-96" alt="" />
+			<div>
+				<p>{image.userId}</p>
+				<p>{image.id}</p>
+				<p>{image.url}</p>x
+			</div>
+		</div>
+	);
 }
+
+// Next img optimization is a bit unhelpful here because we don't know the image specs of user uploaded images
