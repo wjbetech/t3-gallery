834630b5bb77fd2befbf3188aaf1a590edad824f
William East <wjbetech@gmail.com>
2024-06-08 15:32:07 +0900

    Fixed PostHog route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
-import { CSPostHogProvider } from './providers'
+import { CSPostHogProvider } from './_analytics/providers'
 
 // components
 import Navbar from "./_components/Navbar";
@@ -35,30 +35,30 @@ export default function RootLayout({
 }) {
 	return (
 		<ClerkProvider>
-			<html lang="en">
 				<CSPostHogProvider>
-					<NextSSRPlugin
-						/**
-						 * The `extractRouterConfig` will extract **only** the route configs
-						 * from the router to prevent additional information from being
-						 * leaked to the client. The data passed to the client is the same
-						 * as if you were to fetch `/api/uploadthing` directly.
-						 */
-						routerConfig={extractRouterConfig(ourFileRouter)}
-					/>
-					<body className={`font-sans ${inter.variable} dark`}>
-						<div className="h-screen grid grid-rows-[auto,1fr]">
-							<Navbar />
-							<main className="overflow-y-scroll">
-								{children}
-							</main>
-						</div>
-						{modal}
-						<div id="modal-root" />		
-						<Toaster />	
-					</body>
-				</ CSPostHogProvider>
-			</html>
+					<html lang="en">
+							<NextSSRPlugin
+								/**
+								 * The `extractRouterConfig` will extract **only** the route configs
+								 * from the router to prevent additional information from being
+								 * leaked to the client. The data passed to the client is the same
+								 * as if you were to fetch `/api/uploadthing` directly.
+								 */
+								routerConfig={extractRouterConfig(ourFileRouter)}
+							/>
+							<body className={`font-sans ${inter.variable} dark`}>
+								<div className="h-screen grid grid-rows-[auto,1fr]">
+									<Navbar />
+									<main className="overflow-y-scroll">
+										{children}
+									</main>
+								</div>
+								{modal}
+								<div id="modal-root" />		
+								<Toaster />	
+							</body>
+					</html>
+			</ CSPostHogProvider>
 		</ClerkProvider>
 	);
 }
