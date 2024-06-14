fad5b85e52a7d3c55f38ce61471d4560b7f62a91
William East <wjbetech@gmail.com>
2024-06-14 14:25:31 +0900

    Setup reverse proxy in PostHog

--- next.config.js
+++ next.config.js
@@ -19,6 +19,20 @@ const coreConfig = {
 			},
 		],
 	},
+	async rewrites() {
+		return [
+			{
+				source: "/ingest/static/:path*",
+				destination: "https://us-assets.i.posthog.com/static/:path*",
+			},
+			{
+				source: "/ingest/:path*",
+				destination: "https://us.i.posthog.com/:path*",
+			},
+		];
+	},
+	// This is required to support PostHog trailing slash API requests
+	skipTrailingSlashRedirect: true,
 };
 
 import { withSentryConfig } from "@sentry/nextjs";
--- src/app/_analytics/providers.tsx
+++ src/app/_analytics/providers.tsx
@@ -1,43 +1,40 @@
 // app/providers.js
-"use client"
+"use client";
 
-import { useAuth, useUser } from "@clerk/nextjs"
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
-import React, { useEffect } from "react"
+import { useAuth, useUser } from "@clerk/nextjs";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import React, { useEffect } from "react";
 
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-  })
+if (typeof window !== "undefined") {
+	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+		api_host: "/ingest",
+		ui_host: "https://us.posthog.com",
+	});
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-    return (
-      <PostHogProvider client={posthog}>
-        <PostHogAuthWrapper>
-          {children}
-        </PostHogAuthWrapper>
-      </PostHogProvider>
-  );
+	return (
+		<PostHogProvider client={posthog}>
+			<PostHogAuthWrapper>{children}</PostHogAuthWrapper>
+		</PostHogProvider>
+	);
 }
 
 // use clerk with PostHog to identify users
 function PostHogAuthWrapper({ children }: { children: React.ReactNode }) {
-  
-  const auth = useAuth();
-  const userInfo = useUser();
+	const auth = useAuth();
+	const userInfo = useUser();
 
-  useEffect(() => {
-    if (userInfo.user) {
-      posthog.identify(userInfo.user.id, {
-        email: userInfo.user.emailAddresses
-      });
-    } else if (!auth.isSignedIn) {
-      posthog.reset();
-    }
-  }, [auth, userInfo])
+	useEffect(() => {
+		if (userInfo.user) {
+			posthog.identify(userInfo.user.id, {
+				email: userInfo.user.emailAddresses,
+				name: userInfo.user.fullName,
+			});
+		} else if (!auth.isSignedIn) {
+			posthog.reset();
+		}
+	}, [auth, userInfo]);
 
-  return children;
-
+	return children;
 }
